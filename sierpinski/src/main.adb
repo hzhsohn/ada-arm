1311a09e8977f16ec2a266e7adb4faf782030325
han.zh <han.zhihong@qq.com>
2018-10-03 07:12:16 +0800

    Signed-off-by: han.zh <han.zhihong@qq.com>

--- sierpinski/src/main.adb
+++ sierpinski/src/main.adb
@@ -6,13 +6,17 @@ with HAL.Bitmap; use HAL.Bitmap;
 with HAL.Framebuffer; use HAL.Framebuffer;
 with STM32.RNG.Polling; use STM32.RNG.Polling;
 
-procedure Sierpinski is
+procedure main is
    i:Natural:=0;
 begin
 
    Initialize_RNG;
    loop
       i:=i+1;
+      i:=i mod 5;
+      if(i mod 2)=0 then
+        null;
+      end if;
    end loop;
-end Sierpinski;
+end main;
 
