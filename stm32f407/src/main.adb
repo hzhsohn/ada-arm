dc819dc2a43c70454127402a67f57cf2d3fd6062
hzh <hzh.888@qq.com>
2018-11-05 13:10:54 +0800

    Signed-off-by: hzh <hzh.888@qq.com>

--- stm32f407/src/main.adb
+++ stm32f407/src/main.adb
@@ -8,46 +8,19 @@ with Serial_Port;                  use Serial_Port;
 with Last_Chance_Handler;  pragma Unreferenced (Last_Chance_Handler);
 
 
-
 procedure main is
 
-   Uart1_DevBuff : aliased Message (Physical_Size => 1024);  -- arbitrary size
+   ---------------------
+   -- define
+   ---------------------
 
+   isRecvOK: Boolean :=False;
+   uartRecvBuf : aliased Message (Physical_Size => 1024);  -- arbitrary size
+   uartSendBuf : aliased Message (Physical_Size => 1024);  -- arbitrary size
+
+   -- function
 
    procedure Initialize;
-   procedure RebackUartData;
-
-   --------------
-   -- RebackUartData Interact --
-   --------------
-
-   procedure RebackUartData is
-      Received : aliased Message (Physical_Size => 1024);  -- arbitrary size
---  isRecvOK: Boolean :=False;
-   begin
-      Received.Terminator := ASCII.CR;
-
-      -- initizal input message
-      Set (Uart1_DevBuff, To => "Enter text, terminated by CR.");
-      Peripherals.COM.Start_Sending (Uart1_DevBuff'Unchecked_Access);
-      Suspend_Until_True (Uart1_DevBuff.Transmission_Complete);
-
-      -------------------------
-      loop
-         Peripherals.COM.Start_Receiving (Received'Unchecked_Access);
-         Suspend_Until_True (Received.Reception_Complete);
---         --get recv complete status
---         isRecvOK:=Current_State(Received.Reception_Complete);
---         if isRecvOK then
---         end if;
-
-         -- reback uart data
-         Set (Uart1_DevBuff, To => "Received : " & As_String (Received));
-         Peripherals.COM.Start_Sending (Uart1_DevBuff'Unchecked_Access);
-         Suspend_Until_True (Uart1_DevBuff.Transmission_Complete);
-      end loop;
-   end RebackUartData;
-
 
    ----------------
    -- Initialize --
@@ -56,12 +29,34 @@ procedure main is
    procedure Initialize is
    begin
       Peripherals.Initialize_STMicro_UART;
-
+      --uart recv init
+      uartRecvBuf.Terminator := ASCII.CR;
    end Initialize;
+
 
 begin
    Initialize;
-   RebackUartData;
+
+   -- initizal input message
+   Set (uartSendBuf, To => "Enter text, terminated by CR.");
+   Peripherals.COM.Start_Sending (uartSendBuf'Unchecked_Access);
+   Suspend_Until_True (uartSendBuf.Transmission_Complete);
+
+   -------------------------
+   loop
+      Peripherals.COM.Start_Receiving (uartRecvBuf'Unchecked_Access);
+      -- block waiting
+      Suspend_Until_True (uartRecvBuf.Reception_Complete);
+      -- non blocking get. recv complete status
+      isRecvOK:=Current_State(uartRecvBuf.Reception_Complete);
+      if isRecvOK then
+         -- reback uart data
+         Set (uartSendBuf, To => "Received : " & As_String (uartRecvBuf));
+         Peripherals.COM.Start_Sending (uartSendBuf'Unchecked_Access);
+         Suspend_Until_True (uartSendBuf.Transmission_Complete);
+      end if;
+
+   end loop;
 end main;
 
 
